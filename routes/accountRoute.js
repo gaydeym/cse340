e774b219ad7bf993976b2018f0c096afea510c3d
gaydeym <gay20001@byui.com>
2025-04-08 02:06:05 -0500

    W06 Prepare

---
+++ routes/accountRoute.js
@@ -0,0 +1,16 @@
+// Needed Resources 
+const express = require("express")
+const router = new express.Router() 
+const accountController = require("../controllers/accountController")
+const utilities = require("../utilities");
+
+// Deliver login view
+router.get("/login", utilities.handleErrors(accountController.buildLogin))
+
+// Deliver register view
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
+
+// Process registration
+router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+module.exports = router;
