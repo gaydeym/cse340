552cdecb89ff0210b9975fca46a23efe5b9797cb
gaydeym <gay20001@byui.com>
2025-04-08 16:01:16 -0500

    W06 Prepare (part 2)

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation')
 
 // Deliver login view
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
@@ -10,7 +11,22 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin))
 // Deliver register view
 router.get("/register", utilities.handleErrors(accountController.buildRegister))
 
-// Process registration
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+// Process the login attempt
+router.post(
+    "/login",
+    regValidate.loginRules(),
+    regValidate.checkLoginData,
+    (req, res) => {
+        res.status(200).send("login process");
+    }
+);
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
